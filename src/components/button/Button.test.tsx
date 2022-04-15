88f63c800dcf34a088104661c300b0a535854b37
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-15 18:40:35 +0200

    Tests Update

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -7,6 +7,28 @@ test('Button renders the given text', () => {
     const text = "Testing";
 
     const {getByTestId} = render(<Button text={text} variant='primary'></Button>);
-    const my_button = getByTestId('button__txt');
-    expect(my_button.textContent).toBe(text);
+    const my_button__txt = getByTestId('button__txt');
+    expect(my_button__txt.textContent).toBe(text);
+});
+
+test('Primary Button has the right ClassNames', () => {
+   
+    const text = "Testing";
+
+    const {getByTestId} = render(<Button text={text} variant='primary'></Button>);
+    const my_button = getByTestId('button');
+
+    
+    expect(my_button).toHaveClass('my-button--primary');
+});
+
+test('Secondary Button has the right ClassNames', () => {
+   
+    const text = "Testing";
+
+    const {getByTestId} = render(<Button text={text} variant='secondary'></Button>);
+    const my_button = getByTestId('button');
+
+    
+    expect(my_button).toHaveClass('my-button--secondary');
 });
