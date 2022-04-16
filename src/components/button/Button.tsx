1c1a6d60c5ea7af9e9974ac8b6f7c437cbd06b06
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-16 08:58:45 +0200

    built a base for the Button Component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,11 +6,12 @@ import './Button.scss'
 interface Props {
   text: string;
   variant?: 'primary' | 'secondary';
+  round_corners?: boolean;
 }
 
-function Button({text, variant = 'primary'}: Props) {
+function Button({text, variant = 'primary', round_corners = true}: Props) {
   return (
-    <button className={"my-button " + "my-button--" + variant} data-testid="button">
+    <button className={"my-button " + "my-button--" + variant + " " + classNames({"my-button--round-corners" : round_corners})} data-testid="button">
       <p className="my-button__txt" data-testid="button__txt">{text}</p> 
     </button>
   );
