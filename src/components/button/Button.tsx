93a4ff1c07ddc42ecdc9bc8b6fc41a5d820429a4
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-16 09:05:07 +0200

    added the size prop to the Button Compenent

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -7,11 +7,12 @@ interface Props {
   text: string;
   variant?: 'primary' | 'secondary';
   round_corners?: boolean;
+  size?: 'small' | 'normal' | 'large'
 }
 
-function Button({text, variant = 'primary', round_corners = true}: Props) {
+function Button({text, variant = 'primary', round_corners = true, size = 'normal'}: Props) {
   return (
-    <button className={"my-button " + "my-button--" + variant + " " + classNames({"my-button--round-corners" : round_corners})} data-testid="button">
+    <button className={"my-button " + "my-button--" + variant + " " + "my-button--" + size + " " + classNames({"my-button--round-corners" : round_corners})} data-testid="button">
       <p className="my-button__txt" data-testid="button__txt">{text}</p> 
     </button>
   );
