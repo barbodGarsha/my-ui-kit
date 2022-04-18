c848315fd034f4a3517e63b0a629c5be2cffe625
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-18 11:53:21 +0200

    some code cleanup

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 import './Button.scss'
 
-interface Props {
+interface Props{
   text: string;
   variant?: 'primary' | 'secondary';
   round_corners?: boolean;
@@ -11,8 +11,14 @@ interface Props {
 }
 
 function Button({text, variant = 'primary', round_corners = true, size = 'normal'}: Props) {
+  
   return (
-    <button className={"my-button " + "my-button--" + variant + " " + "my-button--" + size + " " + classNames({"my-button--round-corners" : round_corners})} data-testid="button">
+    <button 
+      className={"my-button " + "my-button--" + variant + " " +
+     "my-button--" + size + " " +
+      classNames({"my-button--round-corners" : round_corners})} 
+      data-testid="button">
+
       <p className="my-button__txt" data-testid="button__txt">{text}</p> 
     </button>
   );
