648e7082294054768895631ed2acb1b125b9e880
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-21 11:11:11 +0200

    StoryBook Init

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.scss';
 import Button from './components/button/Button';
 import TextBox from './components/textbox/TextBox';
+import RadioButton from './components/radio-button/RadioButton';
+import RadioButtonItem from './components/radio-button/RadioButtonItem';
 
 function App() {
-  
+  function handle_e(e: React.MouseEvent) {
+    console.log(e.target)
+  }
+
   return (
     <div className="App">
-      <TextBox place_holder='Test' variant='secondary'></TextBox>
+      <RadioButton >
+        <RadioButtonItem onClick={handle_e}></RadioButtonItem>
+        <RadioButtonItem onClick={handle_e}></RadioButtonItem>
+        <RadioButtonItem onClick={handle_e}></RadioButtonItem>
+      </RadioButton>
+      <Button text='CLICK ME' onClick={handle_e}></Button>
     </div>
   );
 }
