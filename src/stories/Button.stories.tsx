928ae30b48a85eef0a8d96d53239f45b71f1c19e
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-21 12:24:15 +0200

    Added stories for Button Component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,52 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import Button from '../components/button/Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+//👇 We create a “template” of how args map to rendering
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  text: 'Click Me',
+  variant: 'primary'
 };
 
 export const Secondary = Template.bind({});
+
 Secondary.args = {
-  label: 'Button',
+  text: 'Click Me',
+  variant: 'secondary'
 };
 
 export const Large = Template.bind({});
+
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  text: 'Click Me',
+  size: 'large'
 };
 
 export const Small = Template.bind({});
+
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  text: 'Click Me',
+  size: 'small'
 };
+
+export const Without_Round_Corners = Template.bind({});
+
+Without_Round_Corners.args = {
+  text: 'Click Me',
+  round_corners: false
+};
+
