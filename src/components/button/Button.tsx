23a71e9d66a59a34a79e9918f83b003f8002a883
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-04-21 10:52:00 +0200

    added events to Button Component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,17 +3,17 @@ import React, { HTMLAttributes } from 'react';
 
 import './Button.scss'
 
-interface Props{
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   text: string;
   variant?: 'primary' | 'secondary';
   round_corners?: boolean;
   size?: 'small' | 'normal' | 'large'
 }
 
-function Button({text, variant = 'primary', round_corners = true, size = 'normal'}: Props) {
+function Button({text, variant = 'primary', round_corners = true, size = 'normal', ...props}: Props) {
   
   return (
-    <button 
+    <button {...props}
       className={"my-button " + "my-button--" + variant + " " +
      "my-button--" + size + " " +
       classNames({"my-button--round-corners" : round_corners})} 
